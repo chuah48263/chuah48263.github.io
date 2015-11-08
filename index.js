49544368615f86ca90022c04697bb2c37a10d6d8
chuah48263 <chuah48263@gmail.com>
2015-11-09 05:13:37 +0800

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var static = require('node-static');
-var file = new static.Server('./index.html');
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        file.serve(request, response);
-    }).resume();
+var file = new static.Server();
+require('http').createServer(function(request, response) {
+  request.addListener('end', function() {
+    file.serve(request, response);
+  }).resume();
 }).listen(8080);
