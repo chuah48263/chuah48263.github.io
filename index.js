05d3ff0e00e19dd99abb98db1ff597ad322a356b
chuah48263 <chuah48263@gmail.com>
2015-11-09 04:57:20 +0800

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 
 var static = require('node-static');
-var file = new static.Server();
-require('http').createServer(function(request, response) {
-  request.addListener('end', function() {
-    file.serve(request, response);
-  }).resume();
-}).listen(process.env.PORT || 3000);
+var file = new static.Server('./');
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        file.serve(request, response);
+    }).resume();
+}).listen(8080);
